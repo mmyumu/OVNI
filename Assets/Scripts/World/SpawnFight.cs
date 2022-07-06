bcc9471727705f1a32818f9e6df74807c7e98e72
mmyumu <mmyumu@gmail.com>
2022-07-07 01:43:46 +0200

    Spawn a fight (square...) on ground (only america and africa) when map is showing

---
+++ Assets/Scripts/World/SpawnFight.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnFight : MonoBehaviour {
+    public GameObject fightPrefab;
+
+    // Start is called before the first frame update
+    void Start() {
+        Bounds mapBoundaries = gameObject.GetComponent<SpriteRenderer>().sprite.bounds;
+        PolygonCollider2D[] polygons = gameObject.GetComponentsInChildren<PolygonCollider2D>();
+
+        bool onGround = false;
+        while(!onGround) {
+            float x = Random.Range(mapBoundaries.min.x, mapBoundaries.max.x);
+            float y = Random.Range(mapBoundaries.min.y, mapBoundaries.max.y);
+
+            Debug.Log($"Try x={x}, y={y}");
+            foreach (PolygonCollider2D polygon in polygons) {
+                if (polygon.OverlapPoint(new Vector2(x, y))) {
+                    onGround = true;
+                    Instantiate(fightPrefab, new Vector3(x, y, 0), Quaternion.Euler(new Vector3(0, 0, 0)) * transform.rotation);
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
